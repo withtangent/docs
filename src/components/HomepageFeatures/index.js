82f6df69efc330647b5bf60232aafbefa2ac274f
Brian Coords <bacoords@gmail.com>
2023-07-23 12:31:53 -0700

    more homepage content ideas

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,75 +1,39 @@
 import React from "react";
-import clsx from "clsx";
 import styles from "./styles.module.css";
-import CodeBlock from "@theme/CodeBlock";
 
 const FeatureList = [
   {
-    title: "A Modern Hybrid Theme with PHP and the Block Editor",
+    title: "A Hybrid Theme with PHP and the Block Editor",
     // Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: <>PHP when you want it, Block Editor everywhere else.</>,
     link: "/functions-php",
-    codeTitle: "footer.php",
-    codeLang: "html",
-    code: `<footer id="colophon" class="site-footer">
-	<?php block_template_part( 'footer-credit' ); ?>
-</footer>`,
   },
   {
     title: "Design Token Approach to Theme.JSON and SCSS",
     // Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: <>Define once, use it everywhere.</>,
     link: "/theme-json-scss",
-    codeTitle: "src/scss/blocks/core/quote.scss",
-    codeLang: "scss",
-    code: `@use "../../utils";
-.wp-block-quote {
-	border: 2px solid utils.ref("preset.color.primary");
-}`,
   },
   {
-    title: "JavaScript for Blocks and the Block Editor",
+    title: "JavaScript Scaffolds for Blocks and the Block Editor",
     // Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: <>Modern JavaScript for modern blocks.</>,
     link: "/javascript",
-    codeTitle: "src/js/editor.js",
-    codeLang: "js",
-    code: `/**
- * Register block styles
- *
- * @type {Object} Add the names of blocks and styles to register here
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-styles/
- */
-const registerBlockStyles = {
-	"core/cover": [
-		{
-			name: "hero",
-			label: "Hero",
-		},
-	]
-};`,
   },
 ];
 
-function Feature({ Svg, title, description, link, codeTitle, codeLang, code }) {
+function Feature({ Svg, title, description, link }) {
   return (
-    <div className="row padding-vert--lg">
-      <div className={clsx("col col--4")}>
-        <div className="">
-          {/* <Svg className={styles.featureSvg} role="img" /> */}
-        </div>
-        <div className=" padding-horiz--md">
-          <h2>{title}</h2>
-          <p>{description}</p>
-          <a className="button button--outline button--primary" href={link}>
-            Keep Reading
-          </a>
-        </div>
+    <div className="padding-vert--lg">
+      <div className="">
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
       </div>
-      <div className={clsx("col col--8")}>
-        <CodeBlock language={codeLang} title={codeTitle}>
-          {code}
-        </CodeBlock>
+      <div className=" padding-horiz--md">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <a className="button button--outline button--primary" href={link}>
+          Keep Reading
+        </a>
       </div>
     </div>
   );
@@ -79,9 +43,13 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        {FeatureList.map((props, idx) => (
-          <Feature key={idx} {...props} />
-        ))}
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <div className="col col--4" key={idx}>
+              <Feature {...props} />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "../components/HomepageFeatures";
+import HomepageFeaturesLarge from "../components/HomepageFeaturesLarge";
 
 export default function Index() {
   return (
@@ -20,13 +21,13 @@ export default function Index() {
             </p>
             <div>
               <a
-                className="button button--primary margin-right--sm"
+                className="button button--primary margin-right--sm button--lg"
                 href="/getting-started"
               >
                 Get Started
               </a>
               <a
-                className="button button--outline button--primary"
+                className="button button--outline button--primary button--lg"
                 href="https://github.com/withtangent/tangent"
               >
                 See the code&nbsp;
@@ -35,7 +36,7 @@ export default function Index() {
                   width="16"
                   height="16"
                   fill="currentColor"
-                  class="bi bi-github"
+                  className="bi bi-github"
                   viewBox="0 0 16 16"
                 >
                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -45,6 +46,35 @@ export default function Index() {
           </div>
         </header>
         <HomepageFeatures />
+        <HomepageFeaturesLarge />
+        <div className="homepage-footer padding-vert--md">
+          <p className="hero__subtitle">
+            Tangent is a modern hybrid theme framework for WordPress, built for
+            developers.
+          </p>
+          <a
+            className="button button--primary margin-right--sm button--lg"
+            href="/getting-started"
+          >
+            Read the docs
+          </a>
+          <a
+            className="button button--outline button--primary button--lg"
+            href="https://github.com/withtangent/tangent"
+          >
+            Get the latest release&nbsp;
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-github"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+            </svg>
+          </a>
+        </div>
       </main>
     </Layout>
   );
